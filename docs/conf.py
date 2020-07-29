ced876c22eee4385bf25bc022cc45b6ad2b6d810
Shane Maloney <shane.maloney@tcd.ie>
2020-07-29 12:43:56 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'STIX-ASW'
+project = 'STIX-GSW'
 copyright = '2020, STIX Team'
 author = 'STIX Team'
 
@@ -49,3 +49,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_extra_path = ['.nojekyll']
